ffcf8ca15a4c295e4598ff50b05d594c123ba8f0
jess <jessica.elessa@gmail.com>
2022-02-16 15:00:24 +0100

    created pages and matching routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Weekly from "./pages/Weekly";
+import WeeklyBattle from "./pages/WeeklyBattle";
+import Popular from "./pages/Home";
+import PopularBattle from "./pages/PopularBattle";
+import Favorites from "./pages/Favorites";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>App</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/weekly" component={Weekly}></Route>
+            <Route exact path="/weekly-battle" component={WeeklyBattle}></Route>
+            <Route exact path="/popular" component={Popular}></Route>
+            <Route
+              exact
+              path="/popular-battle"
+              component={PopularBattle}
+            ></Route>
+            <Route exact path="/favorites" component={Favorites}></Route>
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/pages/Popular.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Popular extends Component {
+  render() {
+    return (
+      <>
+        <h1>Popular</h1>
+      </>
+    );
+  }
+}
