d3e63f38db269e0a3a223dc2e2f7c870cbff5925
jess <jessica.elessa@gmail.com>
2022-02-16 17:14:24 +0100

    added default picture

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,17 @@ export default class Card extends Component {
   render() {
     return (
       <>
-        <img
-          src={`https://image.tmdb.org/t/p/w300/${this.props.poster_path}`}
-          alt="poster"
-        />
+        {this.props.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w300/${this.props.poster_path}`}
+            alt="poster"
+          />
+        ) : (
+          <img
+            src="https://www.pngall.com/wp-content/uploads/1/Film-High-Quality-PNG.png"
+            alt="poster"
+          />
+        )}
         <p>{this.props.title}</p>
         <p>{this.props.release_date}</p>
         <p>{this.props.overview}</p>
