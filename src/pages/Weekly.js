706212063a46e2223fcc50d67909006d60dfae69
jess <jessica.elessa@gmail.com>
2022-02-17 18:24:39 +0100

    part 2 added library moment and variables toaday and lastWeek

--- src/pages/Weekly.js
+++ src/pages/Weekly.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import Card from "../components/Card";
+import moment from "moment";
 
 export default class Weekly extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    const lastWeek = moment().add(-7, "days").format("YYYY-MM-DD"),
+      today = moment().format("YYYY-MM-DD");
+
+    fetch(
+      `http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${lastWeek}&primary_release_date.lte=${today}&api_key=1074a3c6c0b02758af9cae709f3a0e10`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          movies: res.results,
+        });
+        console.log(res);
+      });
+  }
+
   render() {
     return (
       <main>
         <h1>Weekly</h1>
+        {this.state.movies.map((movie) => {
+          return (
+            <Card
+              key={movie.id}
+              poster_path={movie.poster_path}
+              title={movie.title}
+              release_date={movie.release_date}
+              overview={movie.overview}
+            ></Card>
+          );
+        })}
       </main>
     );
   }
