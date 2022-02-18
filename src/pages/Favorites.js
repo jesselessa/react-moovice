528ad9e589d99d0518691f758fa119de9075b4ed
jess <jessica.elessa@gmail.com>
2022-02-18 03:29:23 +0100

    getStorage

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
+import Card from "../components/Card";
 
 export default class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      movie: {},
+      favIds: this.getStorage(),
+    };
+    this.getStorage = this.getStorage.bind(this);
+    this.getMovie = this.getMovie.bind(this);
+  }
+
+  getStorage() {
+    const favMovies = localStorage.getItem("favorites");
+    return JSON.parse(favMovies);
+  }
+
+  // getMovie(id) {
+  //   fetch(
+  //     `http://api.themoviedb.org/3/movie/${id}?api_key=1074a3c6c0b02758af9cae709f3a0e10`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       const mov = this.state.fetchedMovie;
+  //       mov.push(res);
+  //       this.setState({ movies: mov });
+  //     });
+  // }
+
   render() {
     return (
       <>
